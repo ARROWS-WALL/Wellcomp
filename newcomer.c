56c6dbbf36f9e54cba8a4d27c7c2cb26c611edc3
ARROWS-WALL <40079382+ARROWS-WALL@users.noreply.github.com>
2019-12-26 18:57:28 +0900

    Update newcomer.c

--- newcomer.c
+++ newcomer.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-  char a;
+  char name;
   int i = 0;
   int x;
   char number[1][5]=[
@@ -8,8 +8,8 @@
 int main(){
   for(i=0; i<x; i++){
   printf("enter your name = ");
-  scanf("%c", &a);
-  printf("%c", a);
+  scanf("%c", &name);
+  printf("%c", name);
   printf("enter a number = ");
   scanf("%d", &numbers);
   printf("%d", numbers);
