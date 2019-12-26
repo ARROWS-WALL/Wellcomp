83cdc3e862df67460d8e02e1b71fdfea23afe9c8
ARROWS-WALL <40079382+ARROWS-WALL@users.noreply.github.com>
2019-12-26 18:16:38 +0900

    Add files via upload

---
+++ newcomer.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  char a;
+  printf("enter your name = ");
+  scanf("%c", &a);
+  printf("you are%c\n");
+  printf("enter a number = ");
+  scanf("%d", &a);
+  printf("%d");
+}
